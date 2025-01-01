76aedb1398fec783d8b03f2ee913943b4923805b
om <codersarea12@gmail.com>
2025-01-01 16:15:45 +0530

    removed react router dom error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
-  return (
-    <>
-      <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      {/* Other routes */}
-    </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                {/* Default route */}
+                <Route path="/" element={<Login />} /> {/* You can change this to a Home component or any other default page */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                {/* Other routes */}
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-    
-    <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </StrictMode>,
->>>>>>> 26ab246f0db634c25094414d60f4633f9f051c6c
 )
