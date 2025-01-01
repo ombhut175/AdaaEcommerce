6f61ca161969ca091c310d0996b86ea2f75da0a6
om <codersarea12@gmail.com>
2025-01-01 17:04:56 +0530

    removed react router dom error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-
-function App() {
-  return (
-    <>
-      <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      {/* Other routes */}
-    </Routes>
-    </>
-  )
-}
-
-export default App
-=======
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
@@ -38,4 +18,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 9c89c3c (removed react router dom error)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,37 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-    
-    <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
-=======
->>>>>>> 9c89c3c (removed react router dom error)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
->>>>>>> 26ab246f0db634c25094414d60f4633f9f051c6c
-=======
-import {BrowserRouter} from "react-router-dom";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </StrictMode>,
->>>>>>> 9c89c3c (removed react router dom error)
-)
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
