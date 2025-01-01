9962bfff698571b1b1f0d9b87f11ca0b7af88f9d
om <codersarea12@gmail.com>
2025-01-01 16:30:19 +0530

    removed react router dom error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
@@ -16,3 +17,25 @@ function App() {
 }
 
 export default App
+=======
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+    return (
+        <>
+            <Routes>
+                {/* Default route */}
+                <Route path="/" element={<Login />} /> {/* You can change this to a Home component or any other default page */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                {/* Other routes */}
+            </Routes>
+        </>
+    );
+}
+
+export default App;
+>>>>>>> 9c89c3c (removed react router dom error)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,14 +11,27 @@ createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>
 =======
+=======
+>>>>>>> 9c89c3c (removed react router dom error)
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+<<<<<<< HEAD
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 >>>>>>> 26ab246f0db634c25094414d60f4633f9f051c6c
+=======
+import {BrowserRouter} from "react-router-dom";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </StrictMode>,
+>>>>>>> 9c89c3c (removed react router dom error)
 )
