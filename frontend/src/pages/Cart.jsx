86b636fe65895d44c28994892c77b9e4e0e46b6b
om <codersarea12@gmail.com>
2025-01-04 13:23:29 +0530

    frontend connectivity

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const Cart = () => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    console.log(backendUrl);
+    const BACKEND_URL    = import.meta.env.VITE_BACKEND_URL;
     const [cart, setCart] = useState([]);
-    function getCart() {
-        axios.get(`${backendUrl}/api/cart`)
+
+    useEffect(() => {
+        console.log(BACKEND_URL);
+        axios.get(`${BACKEND_URL}/api/cart`)
             .then(res => {
                 setCart(res.data);
             })
-    }
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
     return (
         <div>
             {JSON.stringify(cart)}
