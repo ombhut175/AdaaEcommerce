a278020397304c392eb65469f4a5318911314ce1
yashpatoliya14 <yashpatoliya14@gmail.com>
2025-01-13 16:44:34 +0530

    filtered route completed

--- backend/model/products.js
+++ backend/model/products.js
@@ -2,25 +2,51 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema(
   {
+    dealerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Dealer' },
     name: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String },
     brand: { type: String },
-    dealerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Dealer' },
     price: { type: Number, required: true },
+    categoryOfProduct:{ type:String , required:true },
+    size:[String],
+    color:String,
+    material:String,
     discountPercent: { type: Number },
     stock: { type: Number, default: 0 },
-    reviews: [
-      {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        rating: { type: Number, min: 0, max: 5 },
-        comment: { type: String },
+    reviews: {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
       },
-    ],
-    ratings: {
-      averageRating: { type: Number, min: 0, max: 5, default: 0 },
-      reviewCount: { type: Number, default: 0 },
+      rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+      },
+      comment: {
+        type: String,
+        trim: true, // Remove unnecessary whitespace
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now, // Timestamp for each review
+      },
     },
+    images:{
+      type:String,
+      required:true
+    },
+    features:[String],
+    offers:{
+      bankOffers:String,
+      partnersOffers:String
+    },
+    warrentyDetails:String,
+    addedAt:Date,
+    updatedAt:Date,
+    
+    
   },
   { timestamps: true } 
 );
