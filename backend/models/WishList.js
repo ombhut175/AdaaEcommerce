4cc830328810ab9e212772304cbe7e07f3842cc6
om <codersarea12@gmail.com>
2025-01-25 15:57:58 +0530

    changed wishlist model

--- backend/models/WishList.js
+++ backend/models/WishList.js
@@ -7,12 +7,13 @@ const wishlistSchema = new mongoose.Schema({
         required: true,
         unique: true // Ensure one wishlist per user
     },
-    productId: {
-        type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true
-    }, // Reference to the product
-    addedTimestamp: {type: Date, default: Date.now} // Timestamp when the product was added to the wishlist
-
-}, {timestamps: true});
+    products: [
+        {
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }, // Reference to the product
+            addedTimestamp: { type: Date, default: Date.now } // Timestamp when the product was added to the wishlist
+        }
+    ]
+}, { timestamps: true });
 
 // Create and export the Wishlist model
 const Wishlist = mongoose.model("Wishlist", wishlistSchema);
