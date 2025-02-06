e5ed44816484afd93a590a7378e8f32ebf750663
om <codersarea12@gmail.com>
2025-02-06 15:05:45 +0530

    dealer middleware updated

--- backend/middlewares/dealer.js
+++ backend/middlewares/dealer.js
@@ -1,14 +1,14 @@
-const {giveUserFromDb} = "../services/common.services";
+const {giveUserFromDb} = require("../services/common.services");
 
 async function checkForDealerAuthentication(req, res , next) {
     try {
         const user = await giveUserFromDb(req.cookies.authToken);
-
         if (!user.role.includes("dealer")) {
             return res.status(403).send("Not authorized to authenticate");
         }
         return next();
     }catch (error) {
+        console.log(error);
         return res.status(400).send("Error in Server");
     }
 }
--- backend/middlewares/user.js
+++ backend/middlewares/user.js
@@ -2,8 +2,6 @@ const {giveUserFromDb} = require("../services/common.services");
 
 async function checkForUserAuthentication(req, res, next) {
     try {
-        console.log("from checkForUserAuthentication");
-        console.log(req.cookies.authToken);
         const user = await giveUserFromDb(req.cookies.authToken);
         if (!user.role.includes("customer")) {
             return res.status(401).json({message:'Authentication Failed'});
